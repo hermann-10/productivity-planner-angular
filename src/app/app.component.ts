7146a56ae43168de2789b783dc204d08d12d1e7c
Hermann Manuel <hermann.manuel@hotmail.com>
2024-12-19 17:38:53 +0100

    Live coding N°9 : Walking Skeleton x Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { environment } from '../environments/environment';
+import { RouterOutlet } from '@angular/router';
+import { AuthService } from './core/auth.service';
+import { switchMap } from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+   readonly #authenticationService = inject(AuthService);
    isProductionEnvironment = environment.production;
    firebaseProjectId = environment.firebaseConfig.projectId;
+
+
+   onLogin(){
+    const email = "john.doe@gmail.com";
+    const password = "azerty";
+
+    this.#authenticationService
+      .login(email, password)
+      .pipe(
+        switchMap((response) =>{
+          console.log(response);
+          const { email, localId, idToken} = response;
+          return this.#authenticationService.save(email, localId, idToken);
+        })
+      )
+      .subscribe((response) => console.log(response));
+   }
+
+  //  onRegister(){
+  //   console.log('onRegister');
+  //   const email = "john.doe@gmail.com";
+  //   const password = "azerty";
+
+  //   this.#authenticationService
+  //     .register(email, password).subscribe(console.log);
+  //  }
 }
