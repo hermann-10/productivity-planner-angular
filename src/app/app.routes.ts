fd549fa01b84690355abb788dc9623a411f7e772
Hermann Manuel <63504353+hermann-10@users.noreply.github.com>
2025-01-18 14:22:22 +0100

    feat(home) (#13)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './visitor/home/home.page.component';
+import { RegisterComponent } from './visitor/register/register.component';
+import { LoginComponent } from './visitor/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent, title: 'Productivity Planner' },
+    { path: 'register', component: RegisterComponent, title: 'Register' },
+    { path: 'login', component: LoginComponent, title: 'Login' },
+];
