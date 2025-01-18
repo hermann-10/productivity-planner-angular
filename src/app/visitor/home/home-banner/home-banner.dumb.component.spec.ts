78be2b1166780c1b0b4d4ab929000bbf47179233
Hermann Manuel <hermann.manuel@hotmail.com>
2025-01-18 15:20:56 +0100

    commit5

---
+++ src/app/visitor/home/home-banner/home-banner.dumb.component.spec.ts
@@ -0,0 +1,28 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeBannerDumbComponent } from './home-banner.dumb.component';
+
+describe('HomeBannerDumbComponent', () => {
+  let component: HomeBannerDumbComponent;
+  let fixture: ComponentFixture<HomeBannerDumbComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HomeBannerDumbComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HomeBannerDumbComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it.todo('should display title');
+  it.todo('should display description');
+  it.todo('should display button');
+  it.todo('should trigger event on button click');
+});
--- src/app/visitor/home/home.page.component.ts
+++ src/app/visitor/home/home.page.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { HomeBannerDumbComponent } from './home-banner/home-banner.dumb.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [HomeBannerDumbComponent],
   templateUrl: './home.page.component.html',
   styleUrl: './home.page.component.scss'
 })
 export class HomePageComponent {
+
+  //onXYZ
+  onBannerClicked() {
+    console.log('Banner clicked');
+  }
 
 }
